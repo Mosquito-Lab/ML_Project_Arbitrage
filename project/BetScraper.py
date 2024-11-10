6089c14ad08203323aff5e90daca556f5561ffaf
Smoke <mosquitorepellent127001@gmail.com>
2024-11-10 15:35:36 +0200

    Changing structure of the script

--- project/BetScraper.py
+++ project/BetScraper.py
@@ -4,19 +4,21 @@ from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 import mysql.connector
 
+
 def database():
     db = mysql.connector.connect(host="localhost",
-                                 user= "root",
-                                 password= "",
-                                 database= "")
-    assert db.is_connected() == True
+                                 user="root",
+                                 password="",
+                                 database="")
+    assert db.is_connected() is True
 
     func = db.cursor()
     return db
 
+
 def chrome_driver():
     options = Options()
-    #options.add_experimental_option("detch", True)
+    options.add_experimental_option("detach", True)
 
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
     return driver
@@ -27,51 +29,48 @@ def scraper():
     driver.get("https://www.betway.co.zm")
     driver.maximize_window()
     try:
-        #LOGIN
+        # LOGIN
         number = "976019954"
         password = "VanZ7777"
 
-        #IF THE POPUP PERSISTS
-       # popup = driver.find_element("xpath", "//*[@id='close-toast']").click()
+        # IF THE POPUP PERSISTS
+        # popup = driver.find_element("xpath", "//*[@id='close-toast']").click()
 
-        #SECOND LOGIN POPUP
-        {
-            #some logic comes here
-        }
+        # SECOND LOGIN POPUP
 
-    #LOCATING THE FORM
+        # LOCATING THE FORM
         user_num = driver.find_element("xpath", "//*[@id='header-username']")
         user_pass = driver.find_element("xpath", "//*[@id='header-password']")
         login_button = driver.find_element("xpath", "//*[@id='__nuxt']/div/div/div[1]/header/div/div[2]/div/button[1]")
 
-    #CLICKING TO LOG IN
+        # CLICKING TO LOG IN
         user_num.send_keys(number)
         user_pass.send_keys(password)
         login_button.click()
 
-        #BASKETBALL_EVENTS
+        # BASKETBALL_EVENTS
         basketball_tab = driver.find_element("xpath", "//*[@id='sport-games-list-scroller']/div[4]/button")
-        basketball_tab.click() #Switches to the basketball tab
+        basketball_tab.click()  # Switches to the basketball tab
 
-    
-        basketball_events = driver.find_elements("xpath","//div[@class='rounded_lg'") #Specifies the names and links of episodes
+        basketball_events = driver.find_elements("xpath",
+                                                 "//div[@class='rounded_lg'")  # Where the odds are
         events = []
 
         event_name = driver.find_elements("xpath", "//*[@id='14721451']/div/a")
         home_odds = driver.find_elements("xpath", "//*[@id='14721451']/div/div[1]/div[3]/div/div[1]")
         for event in basketball_events:
-            events.append(event.get_attribute("innerHTML")) #Adds episode to the anime array
-        return  events
+            events.append(event.get_attribute("innerHTML"))  # Adds episode to the anime array
+        return events
 
     finally:
-        driver.quit()
+        # driver.quit()
+        pass
 
     def arbitrage():
         pass
 
-
     def auto_bet():
-            pass
+        pass
 
     return
 
